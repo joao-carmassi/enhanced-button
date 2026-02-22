afcb4751e050d895aa45aa209373e6e7f8bdd58e
Joao Carmassi <joaovitorcarmassi@gmail.com>
2026-02-21 22:04:15 -0300

    remove old button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
 'use client';
 import { Button } from '@/components/ui/button';
-import { CustomButton } from '@/components/ui/custom-button';
 
 export default function Home() {
   return (
     <section className='min-h-screen grid place-items-center'>
-      <div className='flex gap-3'>
-        <Button>Click me</Button>
-        <CustomButton>Click me</CustomButton>
-      </div>
+      <Button>Click me</Button>
     </section>
   );
 }
